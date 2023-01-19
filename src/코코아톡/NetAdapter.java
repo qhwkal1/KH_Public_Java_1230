0a0ea22c304a84f15e80689aa975a783e954cc43
qhwkal1 <qhwkal1@naver.com>
2023-01-19 21:38:17 +0900

    2023-01-19

---
+++ src/코코아톡/NetAdapter.java
@@ -0,0 +1,32 @@
+package 코코아톡;
+
+public interface NetAdapter {
+    void connect();
+    void send(String msg);
+}
+
+class WiFi implements NetAdapter {
+
+    @Override
+    public void connect() {
+
+    }
+
+    @Override
+    public void send(String msg) {
+        System.out.println("WiFi >>" + msg);
+    }
+}
+
+class FiveG implements NetAdapter {
+
+    @Override
+    public void connect() {
+
+    }
+
+    @Override
+    public void send(String msg) {
+        System.out.println("5G >>" + msg);
+    }
+}
